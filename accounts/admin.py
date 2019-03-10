7511c36729819e385dbe03aa53296731fbe25e53
MMD <sirmmds@gmail.com>
2019-03-10 14:18:38 +0330

    add Chequebook, Person, Bank to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,8 @@ from accounts.defaultAccounts.models import DefaultAccount
 from .accounts.models import *
 
 admin.site.register(Account)
+admin.site.register(Bank)
+admin.site.register(Person)
 admin.site.register(FloatAccount)
 admin.site.register(DefaultAccount)
 admin.site.register(AccountType)
