09afbb4b6b07cff67c2f6ee4913388d59fae1b34
MMD <sirmmds@gmail.com>
2019-03-18 07:58:03 +0330

    some helper classes

---
+++ helpers/auth.py
@@ -0,0 +1,34 @@
+from rest_framework.permissions import BasePermission
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+
+
+class BasicCRUDPermission(BasePermission):
+    def has_permission(self, request, view):
+        model = view.serializer_class.Meta.model
+        app_label = model._meta.app_label
+        model_name = model._meta.model_name
+
+        method = request.method
+        user = request.user
+
+        perm = ''
+        if method == 'POST':
+            perm = "{}.create_{}".format(app_label, model_name)
+        if method == 'GET':
+            perm = "{}.retrieve_{}".format(app_label, model_name)
+        if method == 'PUT':
+            perm = "{}.update_{}".format(app_label, model_name)
+        if method == 'DELETE':
+            perm = "{}.delete_{}".format(app_label, model_name)
+
+        return user.has_perm(perm)
+
+
+class TokenAuthSupportQueryString(JSONWebTokenAuthentication):
+    def get_jwt_value(self, request):
+        token = request.query_params.get('token', None)
+        if token:
+            return token
+        else:
+            return super(TokenAuthSupportQueryString, self).get_jwt_value(request)
+
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Permission
 
-# Register your models here.
+from users.models import User, Access
+
+admin.site.register(User)
+admin.site.register(Permission)
+admin.site.register(Access)
+
