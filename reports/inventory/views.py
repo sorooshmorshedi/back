67ad5a5658931a096910a34bf837277e498538f3
MMD <sirmmds@gmail.com>
2019-04-13 18:08:43 +0430

    fix inventory bug

--- reports/inventory/views.py
+++ reports/inventory/views.py
@@ -24,7 +24,7 @@ class InventoryListView(generics.ListAPIView):
             .filter(ware=data['ware'], warehouse=data['warehouse'])\
             .select_related('factor__account')\
             .select_related('factor__sanad')\
-            .order_by('factor__code')
+            .order_by('factor__date', 'factor__time')
 
         ware = Ware.objects.inFinancialYear(request.user).get(pk=data['ware'])
         output_fees = []
