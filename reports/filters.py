02d918af035a7ab15f289cca77544e27f1720215
MMD <sirmmds@gmail.com>
2020-03-22 17:28:00 +0430

    balance sheet without closing sanad filter

--- reports/filters.py
+++ reports/filters.py
@@ -1,5 +1,7 @@
 from django.db.models.query_utils import Q
 
+from helpers.functions import get_current_user
+
 
 def get_account_sanad_items_filter(request) -> Q:
     data = request.GET
@@ -15,5 +17,10 @@ def get_account_sanad_items_filter(request) -> Q:
         filters &= Q(sanadItems__sanad__code__lte=data['to_code'])
     if 'codes' in data:
         filters &= Q(sanadItems__sanad__code__in=data['codes'])
+    if data.get('skip_closing_sanad', False) == 'true':
+        financial_year = get_current_user().active_financial_year
+        closing_sanad = financial_year.closing_sanad
+        if closing_sanad:
+            filters &= ~Q(sanadItems__sanad=closing_sanad)
 
     return filters
