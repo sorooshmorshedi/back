1bd1cd9f32cee8ac220b1766eec5cfd2e1520ca8
MMD <sirmmds@gmail.com>
2020-07-30 11:19:19 +0430

    fix import bugs

--- sanads/views.py
+++ sanads/views.py
@@ -1,13 +1,10 @@
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from mypy.config_parser import Any
 
 from rest_framework import generics
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
