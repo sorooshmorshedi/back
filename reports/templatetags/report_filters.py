bf961ef6612ba69cf8b1a52a5edebc8a1f9c7942
MMD <sirmmds@gmail.com>
2020-08-15 12:15:08 +0430

    refactor export templates

--- reports/templatetags/report_filters.py
+++ reports/templatetags/report_filters.py
@@ -6,6 +6,19 @@ register = template.Library()
 @register.filter(is_safe=True)
 def money(value):
     try:
-        return '{:,}'.format(value)
+        str_value = '{:,}'.format(value)
     except ValueError:
         return value
+    if '.' in str_value:
+        str_value = str_value.strip('0')
+    str_value = str_value.strip('.')
+    return str_value
+
+
+@register.simple_tag
+def colspan(initial_count, *args):
+    count = initial_count
+    for arg in args:
+        if arg:
+            count += 1
+    return count
