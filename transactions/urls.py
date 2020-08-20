91a0fb050717680ba13ca03a921eb0dd9ae58df8
MMD <sirmmds@gmail.com>
2020-08-20 15:25:01 +0430

    fix transaction detail url

--- transactions/urls.py
+++ transactions/urls.py
@@ -3,7 +3,7 @@ from transactions.views import TransactionCreateView, TransactionDetail, Transac
 
 urlpatterns = [
     url(r'^$', TransactionCreateView.as_view(), name=''),
-    url(r'^(?P<pk>[0-9]+)$', TransactionDetail.as_view(), name=''),
+    url(r'^(?P<pk>[0-9]+)/$', TransactionDetail.as_view(), name=''),
     url(r'^(?P<pk>[0-9]+)/confirm/$', ConfirmTransaction.as_view(), name=''),
     url(r'^byPosition$', TransactionByPositionView.as_view(), name=''),
 ]
