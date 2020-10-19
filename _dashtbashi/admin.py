0af1f30c479b418757be41b24b8a6d2ca381dded
MMD <sirmmds@gmail.com>
2020-10-19 09:31:12 +0330

    add oil company lading to admin

--- _dashtbashi/admin.py
+++ _dashtbashi/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from _dashtbashi.models import Car, Driver, Driving, Remittance, Lading, OtherDriverPayment
+from _dashtbashi.models import Car, Driver, Driving, Remittance, Lading, OtherDriverPayment, OilCompanyLading
 
 admin.site.register(Car)
 admin.site.register(Driver)
 admin.site.register(Driving)
 admin.site.register(Remittance)
 admin.site.register(Lading)
+admin.site.register(OilCompanyLading)
 admin.site.register(OtherDriverPayment)
