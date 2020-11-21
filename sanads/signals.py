799d679ff0a8dcffa5e80f2eaac15c7114a66528
MMD <sirmmds@gmail.com>
2020-11-21 10:08:03 +0330

    fix updating balance bug

--- sanads/signals.py
+++ sanads/signals.py
@@ -1,27 +1,24 @@
 from django.db.models import signals
 
 from accounts.accounts.models import AccountBalance
+from helpers.functions import get_object_accounts
 from sanads.models import SanadItem
 
 
 def updateAccountBalanceOnSave(sender, instance: SanadItem, **kwargs):
-    account = instance.account
-    bed = instance.bed
-    bes = instance.bes
-
     if instance.id:
         sanadItem = SanadItem.objects.get(pk=instance.id)
-        bed -= sanadItem.bed
-        bes -= sanadItem.bes
+        AccountBalance.update_balance(
+            **get_object_accounts(sanadItem),
+            bed_change=-sanadItem.bed,
+            bes_change=-sanadItem.bes,
+        )
 
     AccountBalance.update_balance(
-        account=account,
-        bed_change=bed,
-        bes_change=bes,
-        floatAccount=instance.floatAccount,
-        costCenter=instance.costCenter
+        **get_object_accounts(instance),
+        bed_change=instance.bed,
+        bes_change=instance.bes
     )
-    account.save()
 
 
 def updateAccountBalanceOnDelete(sender, instance: SanadItem, **kwargs):
