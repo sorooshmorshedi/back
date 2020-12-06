8b6387f6d6775b83fd63f82eb82d34c98405abb8
MMD <sirmmds@gmail.com>
2020-12-06 13:09:41 +0330

    fix ladings local_ids

--- helpers/views/TestApiView.py
+++ helpers/views/TestApiView.py
@@ -1,7 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from _dashtbashi.models import Lading
+
 
 class TestApiView(APIView):
     def get(self, request):
+        for lading in Lading.objects.filter(financial_year_id=4).order_by('pk'):
+            lading.save()
+
         return Response([])
