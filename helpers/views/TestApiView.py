4f5ee2db8bfa3ee6d8e05e691db6f8a92d2dcf08
MMD <sirmmds@gmail.com>
2020-12-06 13:29:06 +0330

    fix ladings local_ids

--- helpers/views/TestApiView.py
+++ helpers/views/TestApiView.py
@@ -6,7 +6,10 @@ from _dashtbashi.models import Lading
 
 class TestApiView(APIView):
     def get(self, request):
+        i = 1
         for lading in Lading.objects.filter(financial_year_id=4).order_by('pk'):
+            lading.local_id = i
             lading.save()
+            i += 1
 
         return Response([])
