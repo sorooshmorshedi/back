3b1fd48a17018ee2a9b06fca7c8a777ff0e6d2e3
MMD <sirmmds@gmail.com>
2021-02-09 14:26:32 +0330

    add command to update sanad item is_auto_created

--- home/management/commands/run_tmp.py
+++ home/management/commands/run_tmp.py
@@ -1,18 +1,15 @@
 from django.core.management.base import BaseCommand
-from django.db.models import F, Q
 
-from _dashtbashi.models import Lading
-from _dashtbashi.sanads import LadingSanad
-from helpers.middlewares.ModifyRequestMiddleware import ModifyRequestMiddleware
-from users.models import User
+from helpers.test import set_user
+from sanads.models import SanadItem
 
 
 class Command(BaseCommand):
     help = 'Tmp command, for testing, correcting, bug fixing and etc'
 
     def handle(self, *args, **options):
-        ModifyRequestMiddleware.thread_local = type('thread_local', (object,), {'user': User.objects.get(pk=3)})
-
-        for lading in Lading.objects.filter(~Q(sanad_date=F('sanad__date'))).all():
-            print(lading.id)
-            LadingSanad(lading).update()
+        for item in SanadItem.objects.all():
+            print(item.id)
+            set_user(item.created_by)
+            item.is_auto_created = item.sanad.is_auto_created
+            item.save()
