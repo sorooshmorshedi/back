d567c20b60f22030dba04e27c84529db0f5ecd1f
MMD <sirmmds@gmail.com>
2021-03-27 11:08:15 +0430

    add save all factors tmp command

--- home/management/commands/run_tmp.py
+++ home/management/commands/run_tmp.py
@@ -1,17 +1,10 @@
-from decimal import Decimal
-
 from django.core.management.base import BaseCommand
-
-from factors.models import FactorItem
-from helpers.test import set_user
-from users.models import User
-from wares.models import Ware, SalePriceType, SalePrice, Unit
+from factors.models import Factor
 
 
 class Command(BaseCommand):
     help = 'Tmp command, for testing, correcting, bug fixing and etc'
 
     def handle(self, *args, **options):
-        set_user(User.objects.get(pk=1))
-
-        print(SalePrice.objects.filter(conversion_factor=0).update(conversion_factor=1))
+        for item in Factor.objects.all():
+            item.save()
