de3a7e029e0e7f6c8c44595e962868d69472ea50
MMD <sirmmds@gmail.com>
2021-05-31 09:49:25 +0430

    script to find duplicate lading numbers

--- home/management/commands/run_tmp.py
+++ home/management/commands/run_tmp.py
@@ -1,4 +1,7 @@
 from django.core.management.base import BaseCommand
+from django.db.models import Count
+
+from _dashtbashi.models import Lading
 from companies.models import Company, CompanyUser
 from factors.factor_sanad import FactorSanad
 from factors.models import Factor
@@ -9,39 +12,10 @@ class Command(BaseCommand):
     help = 'Tmp command, for testing, correcting, bug fixing and etc'
 
     def handle(self, *args, **options):
-        for company in Company.objects.all():
-            print("Company #{}".format(company.id))
-            superuser = company.superuser
+        qs = Lading.objects.filter(
+            financial_year__company_id=4
+        ).values('lading_number').annotate(
+            count=Count('id')
+        ).values('lading_number').order_by().filter(count__gt=1)
 
-            set_user(superuser)
-
-            if not company.created_by:
-                company.created_by = superuser
-            company.modules = superuser.modules
-            company.save()
-
-            financial_years = company.financial_years.all()
-            users = list(superuser.users.all())
-            users.append(superuser)
-            for user in users:
-                print(" - User #{}".format(user.id))
-                obj, created = CompanyUser.objects.get_or_create(
-                    company=company,
-                    user=user,
-                )
-                obj.financialYears.set(financial_years)
-                obj.roles.set(user.roles.all())
-                print(' -- ', obj, created)
-
-    def update_all_factors_sanads(self):
-        i = 0
-        qs = Factor.objects.filter(is_definite=True)
-        count = qs.count()
-        for factor in qs:
-            print("{}/{} : {}".format(count, i + 1, factor.id))
-            set_user(factor.created_by)
-            try:
-                FactorSanad(factor).update(True)
-            except Exception as e:
-                print(e)
-            i += 1
+        print(list(qs))
