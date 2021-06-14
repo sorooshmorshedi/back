3cfdaaeeb3e5b8e4777943214f4ef034dd95a77b
MMD <sirmmds@gmail.com>
2021-06-14 10:58:54 +0430

    add distribution models to admin

--- distributions/admin.py
+++ distributions/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from distributions.models import Visitor, CommissionRange, Path
+from distributions.models.car_model import Car
+from distributions.models.distribution_model import Distribution
+from distributions.models.distributor_model import Distributor
+from distributions.models.driver_model import Driver
+
+admin.site.register(Visitor)
+admin.site.register(Car)
+admin.site.register(CommissionRange)
+admin.site.register(Distribution)
+admin.site.register(Distributor)
+admin.site.register(Driver)
+admin.site.register(Path)
