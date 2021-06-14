5e5f95a5611e77a749ab0e12451d184dbfa4e618
MMD <sirmmds@gmail.com>
2021-06-14 10:13:18 +0430

    add script to fix multiple first period inventories

--- home/management/commands/run_tmp.py
+++ home/management/commands/run_tmp.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.db.models import Count
 
 from _dashtbashi.models import Lading
-from companies.models import Company, CompanyUser
+from companies.models import Company, CompanyUser, FinancialYear
 from factors.factor_sanad import FactorSanad
 from factors.models import Factor
 from helpers.test import set_user
@@ -12,10 +12,11 @@ class Command(BaseCommand):
     help = 'Tmp command, for testing, correcting, bug fixing and etc'
 
     def handle(self, *args, **options):
-        qs = Lading.objects.filter(
-            financial_year__company_id=4
-        ).values('lading_number').annotate(
-            count=Count('id')
-        ).values('lading_number').order_by().filter(count__gt=1)
-
-        print(list(qs))
+        for financial_year in FinancialYear.objects.all():
+            print("#{}".format(financial_year.id))
+            factors = list(Factor.objects.inFinancialYear(financial_year).filter(code=0).order_by('id').all())
+            if len(factors) > 2:
+                for i in range(1, len(factors)):
+                    factor = factors[i]
+                    print(" - #{}".format(factor.id))
+                    factor.delete()
