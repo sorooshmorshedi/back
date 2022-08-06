75e62cb102b4be459a5681c2b283934ab80af045
soroosh morshedi <devolopersoroosh@gmail.com>
2022-08-06 10:15:36 +0430

    add list views for payroll module

--- payroll/lists/views.py
+++ payroll/lists/views.py
@@ -1,11 +1,87 @@
-class TenderListView(generics.ListAPIView):
+from rest_framework import generics
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticated
+
+from helpers.auth import BasicCRUDPermission
+from payroll.lists.filters import WorkshopFilter, PersonnelFilter, PersonnelFamilyFilter, WorkshopPersonnelFilter, \
+    ContractRowFilter, ContractTimeFilter
+from payroll.models import Workshop, Personnel, PersonnelFamily, WorkshopPersonnel, ContractRow, ContractTime
+from payroll.serializers import WorkShopSerializer, PersonnelSerializer, PersonnelFamilySerializer, \
+    WorkshopPersonnelSerializer, ContractRowSerializer, ContractTimeSerializer
+
+
+class WorkshopListView(generics.ListAPIView):
     permission_classes = (IsAuthenticated, BasicCRUDPermission)
 
-    permission_codename = "get.tender"
-    serializer_class = TenderSerializer
-    filterset_class = TenderFilter
+    permission_codename = "get.workshop"
+    serializer_class = WorkShopSerializer
+    filterset_class = WorkshopFilter
     ordering_fields = '__all__'
     pagination_class = LimitOffsetPagination
 
     def get_queryset(self):
-        return Tender.objects.hasAccess('get', self.permission_codename).all()
+        return Workshop.objects.hasAccess('get', self.permission_codename).all()
+
+
+class PersonnelListView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated, BasicCRUDPermission)
+
+    permission_codename = "get.personnel"
+    serializer_class = PersonnelSerializer
+    filterset_class = PersonnelFilter
+    ordering_fields = '__all__'
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        return Personnel.objects.hasAccess('get', self.permission_codename).all()
+
+
+class PersonnelFamilyListView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated, BasicCRUDPermission)
+
+    permission_codename = "get.personnel_family"
+    serializer_class = PersonnelFamilySerializer
+    filterset_class = PersonnelFamilyFilter
+    ordering_fields = '__all__'
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        return PersonnelFamily.objects.hasAccess('get', self.permission_codename).all()
+
+
+class WorkshopPersonnelListView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated, BasicCRUDPermission)
+
+    permission_codename = "get.workshop_personnel"
+    serializer_class = WorkshopPersonnelSerializer
+    filterset_class = WorkshopPersonnelFilter
+    ordering_fields = '__all__'
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        return WorkshopPersonnel.objects.hasAccess('get', self.permission_codename).all()
+
+
+class ContractRowListView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated, BasicCRUDPermission)
+
+    permission_codename = "get.contract_row"
+    serializer_class = ContractRowSerializer
+    filterset_class = ContractRowFilter
+    ordering_fields = '__all__'
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        return ContractRow.objects.hasAccess('get', self.permission_codename).all()
+
+
+class ContractTimeListView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated, BasicCRUDPermission)
+
+    permission_codename = "get.contract_time"
+    serializer_class = ContractTimeSerializer
+    filterset_class = ContractTimeFilter
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        return ContractTime.objects.hasAccess('get', self.permission_codename).all()
